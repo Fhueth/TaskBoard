27109bba9e39cdf323e15c28c8b71d2247b2b365
Fhueth <alexperezvera2006@gmail.com>
2026-02-13 15:38:46 +0100

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Form from './components/Form.vue';
+import ToDoTaskList from './components/ToDoTaskList.vue';
+
+</script>
+
+
+<template>
+    <body class="bg-amber-100 w-dvw h-dvh">
+        <Form/>
+        <ToDoTaskList/>
+    </body>
+</template>
---
+++ src/components/Form.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Button from './form/Button.vue';
+import Input from './form/Input.vue';
+
+
+</script>
+
+<template>
+    <form @submit.prevent="">
+        <Input/>
+        <Button/>
+    </form>
+</template>

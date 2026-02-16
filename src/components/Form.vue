4dd67f9b879882dacffd7b7aa876b2a5d3df05da
Fhueth <alexperezvera2006@gmail.com>
2026-02-16 15:20:31 +0100

    definido props y emits entre formulario y campo

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
 import Button from './form/Button.vue';
-import Input from './form/Input.vue';
+import Input from './form/Input.vue';   
+import type { Field } from '../types/Form';
 
+const form = reactive({
+    name: { value: '', valid: false },
+});
+
+const check = (field: Field, valid: boolean) => {
+    form[field].valid = valid
+}
+const submit = ()=> {
+    if(!Object.values(form).every((f) => f.valid)) {
+        return;
+    }
+}
 
 </script>
 
 <template>
-    <form @submit.prevent="">
-        <Input/>
+    <form @submit.prevent="submit" novalidate
+    class="flex gap-2 items-center size-fit backdrop-blur-2xl fixed top-10 left-1/2 -translate-x-1/2 w-fit z-50"
+    >
+        <Input @validity="check" type="text" name="name" required>Nombre</Input>
         <Button/>
     </form>
 </template>

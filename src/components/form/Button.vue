f3aa0936f4bf744b62823a721ff3bdd659578090
Fhueth <alexperezvera2006@gmail.com>
2026-02-16 15:20:09 +0100

    añadido vue para botones (de momento añadir)

--- src/components/form/Button.vue
+++ src/components/form/Button.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-
+import Icon from './Icon.vue';
 </script>
 
 <template>
-    
+    <button title="añadir" type="submit" class="p-3.5 rounded-full bg-blue-500 size-fit shadow-2xl cursor-pointer hover:scale-110 transition duration-150">
+        <Icon type="add"/>
+    </button>
 </template>

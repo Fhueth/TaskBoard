78c5da14569917c143cf632ff9a89e060976f444
Fhueth <alexperezvera2006@gmail.com>
2026-02-16 15:19:37 +0100

    añadido un archivo dinamico para iconos

---
+++ src/components/form/Icon.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+    type: string
+}>()
+
+</script>
+
+<template>
+    <svg class="icon size-8">
+        <use :href="`/icons/sprite.svg#${type}`"></use>
+    </svg>
+</template>

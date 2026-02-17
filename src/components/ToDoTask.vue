b841c7c0ce29cb164ff421fe3d737260277aed2c
Fhueth <alexperezvera2006@gmail.com>
2026-02-17 19:30:18 +0100

    controlado localstorage a las tareas para guardar y obtener tareas

--- src/components/ToDoTask.vue
+++ src/components/ToDoTask.vue
@@ -9,10 +9,9 @@ const props = defineProps<{
 </script>
 
 <template>
-    <article class="bg-fuchsia-200">
-        <p>ID: {{ task.id }}</p>
-        <p>Título: {{ task.title }}</p>
-        <p>Estado: {{ task.done }}</p>
-        <p>Fecha: {{ task.createdAt }}</p>
+    <article :class="`font-mono ${task.done ? `bg-emerald-500` : `bg-red-400`} min-w-2xs min-h-2xs py-10 px-6 rounded-xl flex flex-col gap-2 justify-center items-center shadow-2xl relative`">
+        <span class="absolute top-0 left-1"> {{ task.id }} </span>
+        <h1 class="text-2xl">{{ task.title }}</h1>
+        <p>{{ task.createdAt }}</p>
     </article>
 </template>9

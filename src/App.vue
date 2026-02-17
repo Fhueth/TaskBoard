2106fc6cb67d5e7c6494e817de3ed0c4ed7d5cc1
Fhueth <alexperezvera2006@gmail.com>
2026-02-17 14:42:22 +0100

    añadido funcion para enviar la tarea recibida

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import Form from './components/Form.vue';
 import ToDoTaskList from './components/ToDoTaskList.vue';
 
+const data = ref<string>('');
+const setTask = (title: string)=> {
+    data.value = title;
+}
 </script>
 
 
 <template>
-    <Form/>
-    <p class="w-fit text-black mt-20">asdf</p>
-    <ToDoTaskList/>
+    <Form @title="setTask"/>
+    <ToDoTaskList class="mt-20" :title="data"/>
 </template>

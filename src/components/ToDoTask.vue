99528d2e2ac96d94f15098f709ab3734690f6019
Fhueth <alexperezvera2006@gmail.com>
2026-02-17 14:43:04 +0100

    añadido vue para renderizar tarea

--- src/components/ToDoTask.vue
+++ src/components/ToDoTask.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import type { Task } from '../types/Task';
+
+
+const props = defineProps<{
+    task: Task,
+}>();
 
 </script>
 
 <template>
-    
-</template>
+    <article class="bg-fuchsia-200">
+        <p>ID: {{ task.id }}</p>
+        <p>Título: {{ task.title }}</p>
+        <p>Estado: {{ task.done }}</p>
+        <p>Fecha: {{ task.createdAt }}</p>
+    </article>
+</template>9

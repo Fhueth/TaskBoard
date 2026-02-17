d0e8b59c7639443d08878cd211a28367938a4d73
Fhueth <alexperezvera2006@gmail.com>
2026-02-17 14:42:50 +0100

    añadido script para añadir nueva tarea recibida y renderizarla

--- src/components/ToDoTaskList.vue
+++ src/components/ToDoTaskList.vue
@@ -1,7 +1,30 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue';
+import type { Task } from '../types/Task';
+import ToDoTask from './ToDoTask.vue';
+
+const id = ref<number>(1);
+const taskList = ref<Task[]>([]);
+
+const props = defineProps<{
+    title: string,
+}>();
+
+watch(()=>props.title, (title: string)=> {
+    if (title != '') {
+        taskList.value.push({
+            id: ++id.value,
+            title: title,
+            done: false,
+            createdAt: 23,
+        });
+    }
+});
 
 </script>
 
 <template>
-    
+    <section class="flex flex-wrap gap-2.5">
+        <ToDoTask v-for="task in taskList" :key="task.id" :task="task"/>
+    </section>
 </template>

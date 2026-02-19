2f1b2b04230d2128696eb4f1b5a9a0cea8e702fd
Fhueth <alexperezvera2006@gmail.com>
2026-02-19 13:23:40 +0100

    añadido eventos de teclas para guardar y salir de edicion

--- src/components/ToDoTask.vue
+++ src/components/ToDoTask.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useEventListener } from '@vueuse/core';
 import type { Task } from '../types/Task';
 import Icon from './form/Icon.vue';
 
@@ -7,10 +8,11 @@ const isEditing = ref(false);
 const props = defineProps<{
     task: Task,
 }>();
+const newValue = ref<string>(props.task.title);
 
 const emit = defineEmits<{
-    (e: 'deleteTask', id: number): void
-    (e: 'editTask', id: number, task: Task): void
+    deleteTask: [id: number]
+    editTask: [id: number, task: Task]
 }>();
 const deleteTask = ()=> {
     emit('deleteTask', props.task.id)
@@ -18,9 +20,16 @@ const deleteTask = ()=> {
 const editTask = ()=> {
     isEditing.value = true;
 }
-const sendNewValue = ()=> {
-    isEditing.value = false;
-}
+
+useEventListener('keydown', (e: KeyboardEvent) => {
+    if(e.key === 'Enter') {
+        emit('editTask', props.task.id, props.task)
+        isEditing.value = false;
+    }
+    else if(e.key === 'Escape') {
+        isEditing.value = false;
+    }
+})
 </script>
 
 <template>
@@ -28,13 +37,11 @@ const sendNewValue = ()=> {
         <button @click="deleteTask" class="size-6 absolute top-1 right-1 transition-transform duration-150 hover:scale-110 cursor-pointer">
             <Icon class="size-full" type="delete"/>
         </button>
-        <span class="absolute top-0 left-1"> {{ task.id }} </span>
         <h1 class="text-2xl">{{ task.title }}</h1>
         <p>{{ task.createdAt }}</p>
     </article>
-    <article v-else @keyup.enter.prevent="sendNewValue" :class="`font-mono ${task.done ? `bg-emerald-500` : `bg-red-400`} min-w-2xs min-h-2xs py-10 px-6 rounded-xl flex flex-col gap-2 justify-center items-center shadow-2xl relative`">
-        <span class="absolute top-0 left-1"> {{ task.id }} </span>
-        <input :value="`${ task.title }`" class="text-2xl">
-        <p>{{ task.createdAt }}</p>-
+    <article v-else :class="`font-mono ${task.done ? `bg-emerald-500` : `bg-red-400`} min-w-2xs min-h-2xs py-10 px-6 rounded-xl flex flex-col gap-2 justify-center items-center shadow-2xl relative`">
+        <input v-model="newValue" class="text-2xl">
+        <p>{{ task.createdAt }}</p>
     </article>
 </template>9

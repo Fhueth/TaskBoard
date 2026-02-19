6f7198045ee873429971c76eefbe31ce96c8ef6c
Fhueth <alexperezvera2006@gmail.com>
2026-02-19 13:24:43 +0100

    cambiado listas por un Map y manejado la creacion de tareas con una funcion y no un watch

--- src/components/ToDoTaskList.vue
+++ src/components/ToDoTaskList.vue
@@ -1,40 +1,47 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import type { Task } from '../types/Task';
 import ToDoTask from './ToDoTask.vue';
 
-const taskList = ref<Task[]>([]);
-const id = ref<number>(taskList.value.length === 0 ? 0 : taskList.value.length-1);
-const props = defineProps<{
-    title: string,
-}>();
-
+const taskList = ref<Map<number, Task>>(new Map());
 const save = () => {
-    localStorage.setItem('tasks', JSON.stringify(taskList.value));
+    localStorage.setItem('tasks', JSON.stringify(arrayTasks.value));
 }
-watch(()=>props.title, (title: string)=> {
-    if (title != '') {
-        const tasksList: Task = {
-            id: id.value++,
-            title: title,
-            done: false,
-            createdAt: new Date().toISOString().split('T')[0] ?? '',
-        };
-        taskList.value.push(tasksList);
-        save();
-    }
-});
+const get = () => {
+    return new Map(
+        (JSON.parse(localStorage.getItem('tasks') || '[]') as Task[]).map(task => [task.id, task])
+    );
+}
+const addTask = (title: string)=> {
+    const id = Date.now();
+    const tasksList: Task = {
+        id: id,
+        title: title,
+        done: false,
+        createdAt: new Date().toISOString().split('T')[0] ?? '',
+    };
+    taskList.value.set(Date.now(), tasksList);
+    save();
+}
+const arrayTasks = computed(()=>Array.from(taskList.value.values()))
+defineExpose({
+    addTask
+})
+const saveTask = (id: number, task: Task)=> {
+    taskList.value.set(id, task);
+}
 const deleteTaskFromList = (id: number)=> {
-    taskList.value.splice(taskList.value.findIndex(task => task.id === id), 1);
+    taskList.value.delete(id);
     save();
 }
 onMounted(()=>{
-    taskList.value = JSON.parse(localStorage.getItem('tasks') || '[]');
+    taskList.value = get();
 })
+
 </script>
 
 <template>
     <section class="flex flex-wrap gap-2.5 overflow justify-center items-center p-10">
-        <ToDoTask @deleteTask="deleteTaskFromList" v-for="task in taskList" :key="task.id" :task="task"/>
+        <ToDoTask @editTask="saveTask" @deleteTask="deleteTaskFromList" v-for="(task, index) in arrayTasks" :key="index+1" :task="task"/>
     </section>
 </template>

2141da08082ae0a8bd739a18f288708870ea4f0d
Fhueth <alexperezvera2006@gmail.com>
2026-02-23 16:42:52 +0100

    incluido color como variable dinamica prop

--- src/components/form/Icon.vue
+++ src/components/form/Icon.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 const props = defineProps<{
-    type: string
+    type: string,
+    color: string,
 }>()
 
 </script>
 
 <template>
-    <svg class="size-8 text-white" aria-hidden="true">
+    <svg class="size-8" :color="`${color ? color : 'currentColor'}`" aria-hidden="true">
         <use :href="`/icons/sprite.svg#${type}`" :xlink:href="`/icons/sprite.svg#${type}`" />
     </svg>
 </template>
